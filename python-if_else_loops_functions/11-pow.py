41409112dbaf7abcac059d0b55ed8ba403aec450
Mathieu7483 <11436@holbertonstudents.com>
2025-09-01 18:03:54 +0200

    Refactor pow function to simplify exponentiation logic using built-in operator

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return 1
-    elif b > 0:
-        resultat = 1
-        for i in range(b):
-            resultat *= a
-        return resultat
-    else:
-        resultat = 1
-        for i in range(abs(b)):
-            resultat *= (a)
-    return 1/resultat
+    power = a**b
+    return power
