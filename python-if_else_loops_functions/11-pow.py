98fb4cea2c5ab877226c465465600d7fc60e2c75
Mathieu7483 <11436@holbertonstudents.com>
2025-09-01 17:50:08 +0200

    Refactor pow function to implement manual exponentiation logic

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def pow(a, b):
-    return (a ** b)
+    if b == 0:
+        return 1
+    elif b > 0:
+        resultat = 1
+        for i in range(b):
+            resultat *= a
+        return resultat
+    else:
+        resultat = 1
+        for i in range(abs(b)):
+            resultat *= (a)
+    return 1/resultat
