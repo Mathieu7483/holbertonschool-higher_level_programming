ead01008af98317188256993683b0617b617382c
Mathieu7483 <11436@holbertonstudents.com>
2025-09-02 13:31:52 +0200

    Fix string formatting in 1-calculation.py to use format method instead of f-string

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,8 +3,8 @@ import calculator_1
 
 if __name__ == "__main__":
     a = 10
-    b = 5 
-    print(f"{a} + {b} = {calculator_1.add(a, b)}")
-    print(f"{a} - {b} = {calculator_1.sub(a, b)}")
-    print(f"{a} * {b} = {calculator_1.mul(a, b)}")
-    print(f"{a} / {b} = {calculator_1.div(a, b)}")
+    b = 5
+    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
