d874f87c144bdc7f3f288c6cd4b2f4e50e479d74
Mathieu7483 <11436@holbertonstudents.com>
2025-09-04 15:21:13 +0200

    Refactor 100-print_tebahpla.py to use a loop for character printing

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print("".join([chr(122 - i).lower() if i % 2 == 0 else chr(122 - i).upper() for i in range(26)]), end="")
+for i in range(ord("z"), ord("a") - 1, -1):
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{}".format(chr(i - 32)), end="")
