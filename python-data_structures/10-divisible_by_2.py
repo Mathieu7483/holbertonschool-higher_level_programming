cca44435defcc7c19f1fcb5f3d5598bf6daad476
Mathieu7483 <11436@holbertonstudents.com>
2025-09-08 11:54:10 +0200

    Implement divisible_by_2 function to check divisibility by 2 in a list

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return None
+ 
+    result_list = []
+
+    for number in my_list:
+        if number % 2 == 0:
+            result_list.append(True)
+        else:
+            result_list.append(False)
+    
+    return result_list
