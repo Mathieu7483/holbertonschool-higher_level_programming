29499b9948374041504effb2d4b98ea828817711
Mathieu7483 <11436@holbertonstudents.com>
2025-09-08 14:58:34 +0200

    Refactor multiply_by_2 function to create a new dictionary instead of modifying the input

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:    
-        a_dictionary[key] *=  2
+    new_dict = {}
+    for key, value in a_dictionary.items():    
+        new_dict[key] = value * 2
+    return new_dict
