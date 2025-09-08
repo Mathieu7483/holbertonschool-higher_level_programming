10cf87fdff3e78796e488828a0d60785a06a3e70
Mathieu7483 <11436@holbertonstudents.com>
2025-09-08 13:23:04 +0200

    Fix indentation in search_replace function for consistency

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for index in range(len(new_list)):
-            if new_list[index] == search:
-                new_list[index] = replace
+        if new_list[index] == search:
+            new_list[index] = replace
     return new_list
