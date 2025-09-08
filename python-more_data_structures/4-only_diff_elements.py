0f80328b4c98cc719279e4ac994d3792dedd0023
Mathieu7483 <11436@holbertonstudents.com>
2025-09-08 14:17:34 +0200

    Refactor only_diff_elements function to use set symmetric difference for improved efficiency

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    result = []
-    for element in set_1:
-        if element not in set_2:
-            result.append(element)
-    return result
+    return set_1 ^ set_2
