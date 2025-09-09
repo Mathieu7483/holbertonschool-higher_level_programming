da0e3c894d0ea3c4f0d6bc2f76509e0d7e1b16dd
Mathieu7483 <11436@holbertonstudents.com>
2025-09-09 10:45:11 +0200

    Refactor safe_print_integer function to handle TypeError in addition to ValueError

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-""" Safe printing of an integers list """
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
-
