4e1cd684b9c3805ea0fcfc7c0147a2cc945cf902
Mathieu7483 <11436@holbertonstudents.com>
2025-09-09 12:47:36 +0200

    Refactor list_division function to improve error handling and return new_list correctly

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-""" Divide a list """
 def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        result = 0
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except (TypeError, ValueError):
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            return new_list
+
