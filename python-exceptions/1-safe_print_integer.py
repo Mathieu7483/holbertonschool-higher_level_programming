58e0d4b180967ed213a79dd17ce96ac0ef076803
Mathieu7483 <11436@holbertonstudents.com>
2025-09-09 10:42:41 +0200

    Refactor safe_print_integer function to enhance error handling and maintainability

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 """ Safe printing of an integers list """
 def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
+
