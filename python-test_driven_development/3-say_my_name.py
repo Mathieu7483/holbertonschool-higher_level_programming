a98e5890c17090d6dae6937fbf70e4e5531e12a3
Mathieu7483 <11436@holbertonstudents.com>
2025-09-10 15:35:16 +0200

    Fix formatting in say_my_name function docstring and print statement

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,12 +3,13 @@
 Module that define a print of a first name and last name
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
-     function that prints My name is <first name> <last name> 
+    function that prints My name is <first name> <last name>
     """
     if not isinstance(first_name, (str)):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, (str)):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format (first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
