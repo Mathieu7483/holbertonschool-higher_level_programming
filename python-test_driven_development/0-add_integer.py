5a2ff9029a4223bca2eeffee0d68911ba103b5fa
Mathieu7483 <11436@holbertonstudents.com>
2025-09-10 10:01:17 +0200

    Update docstring in add_integer function to clarify parameter types

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,6 +2,8 @@
 def add_integer(a, b=98):
 	"""
 	Returns an integer: the addition of a and b
+	a must be an integer
+	b must be an integer
 	"""
 	if not isinstance(a, (int, float)):
 		raise TypeError("a must be an integer")
