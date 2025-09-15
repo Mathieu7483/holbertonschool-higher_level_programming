3d7b1e53fafb34be28fa4ddfe982756fa768cfa1
Mathieu7483 <11436@holbertonstudents.com>
2025-09-15 09:50:09 +0200

    Fix constructor to use size setter for proper validation in Square class

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Class who define an square with size argument."""
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
