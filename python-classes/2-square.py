8013bf8fe5386cfadc4d312e0b3d1aecf7127317
Mathieu7483 <11436@holbertonstudents.com>
2025-09-15 08:59:13 +0200

    Add Square class with size validation and error handling

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module who define a square class."""
+
+
+class Square:
+    """Class who define an square with size argument."""
+    def __init__(self, size = 0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
