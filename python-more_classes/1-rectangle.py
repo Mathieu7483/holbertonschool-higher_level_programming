ed884c6571ef3ccc54db3e07b4a10cc7afc3b7a3
Mathieu7483 <11436@holbertonstudents.com>
2025-09-15 13:47:49 +0200

    Fix width and height property setters in Rectangle class

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,8 +18,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__size = value
-
+        self.__width = value
 
     @property
     def height(self):
@@ -28,8 +27,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__size = value
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
