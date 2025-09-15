ad42ec3dc6596d2d03ed70b12a93057eba111a75
Mathieu7483 <11436@holbertonstudents.com>
2025-09-15 09:45:45 +0200

    Add 4-square.py module defining Square class with area method

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module who define a square class."""
+
+
+class Square:
+    """Class who define an square with size argument."""
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        self.area = self.__size * self.__size
+        return self.area
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
