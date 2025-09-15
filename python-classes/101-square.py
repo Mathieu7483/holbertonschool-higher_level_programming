f511bab03990ebdc3c8e944eca0ef5d668b1d0e4
Mathieu7483 <11436@holbertonstudents.com>
2025-09-15 12:47:54 +0200

    Implement Square class with area calculation and string representation

---
+++ python-classes/101-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Module who define a square class."""
+
+
+class Square:
+    """Class who define an square with size argument."""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        print(self.__str__())
+
+    def __str__(self):
+        if self.__size == 0:
+            return ""
+        square_string = ""
+        square_string += "\n" * self.__position[1]
+
+        for i in range(self.__size):
+            line = " " * self.__position[0] + "#" * self.__size
+            square_string += line + "\n"
+
+            return square_string[:-1]
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) and num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
