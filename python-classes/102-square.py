f25baa5957e4c3412610345b93061860a82e8f86
Mathieu7483 <11436@holbertonstudents.com>
2025-09-15 13:15:42 +0200

    Implement Square class with size validation and area calculation

---
+++ python-classes/102-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Module who define a square class."""
+
+
+class Square:
+    """Class who define an square with size argument."""
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (float, int)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        if isinstance(other, Square):
+            return self.area() == other.area()
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, Square):
+            return self.area() != other.area()
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Square):
+            return self.area() > other.area()
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, Square):
+            return self.area() >= other.area()
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Square):
+            return self.area() < other.area()
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, Square):
+            return self.area() <= other.area()
+        return NotImplemented
