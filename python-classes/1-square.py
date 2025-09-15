d66c37dec36a01969ac54b833fcc2879e97cd675
Mathieu7483 <11436@holbertonstudents.com>
2025-09-15 08:52:25 +0200

    Add initial implementation of Square class with size attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module who define a square class."""
+
+
+class Square:
+    """Class who define an square with size argument."""
+    def __init__(self, size):
+        self.__size = size
