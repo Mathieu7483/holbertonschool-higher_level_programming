9b1032049f7fab1dd420d0c4eb8eb28ef51eac45
Mathieu7483 <11436@holbertonstudents.com>
2025-09-15 08:38:35 +0200

    Refactor Square class to be empty and remove width, height attributes and methods

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,9 +2,5 @@
 """Write an empty class Square that defines a square"""
 
 
-class square:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-    def is_square(self):
-        return self.width == self.height
+class Square:
+    pass
