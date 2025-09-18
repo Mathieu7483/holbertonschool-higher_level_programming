b56d42830833636bd0c51fdd045abc6258fb6748
Mathieu7483 <11436@holbertonstudents.com>
2025-09-18 14:38:16 +0200

    Add is_kind_of_class function to check class type

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that define a kind of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Class that define a kind of"""
+    return isinstance(type(obj) is a_class)
