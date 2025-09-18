c61a4c0d88894f305ed38373dd5cb2a8eff21b93
Mathieu7483 <11436@holbertonstudents.com>
2025-09-18 14:49:16 +0200

    Fix inherits_from function to correctly check for class inheritance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Class that define a inherits from"""
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
