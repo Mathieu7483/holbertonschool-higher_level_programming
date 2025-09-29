a4d4ed8147bfd057ca68a9633a940503ed19fdc3
Mathieu7483 <11436@holbertonstudents.com>
2025-09-29 09:16:30 +0200

    Implement write_file function to create and write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that define a write"""
+
+
+def write_file(filename="", text=""):
+    """function that write a file"""
+
+    with open(filename="", text="") as f:
+        f.write(filename)
