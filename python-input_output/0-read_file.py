eeebec7719b7a0eb7dcb3bf2c3ce1bbe5ff25804
Mathieu7483 <11436@holbertonstudents.com>
2025-09-29 08:57:29 +0200

    Add read_file function to read and print file contents

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that define a read"""
+
+
+def read_file(filename=""):
+    with open('my_file_0.txt', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
