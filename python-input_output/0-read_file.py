d7a89074413f1c025a612e7ca06dae6a5cae0843
Mathieu7483 <11436@holbertonstudents.com>
2025-09-29 09:08:29 +0200

    Update file name in read_file function to match expected input

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """function that read a file"""
 
-    with open('my_file_0.txt', encoding="utf-8") as f:
+    with open('file_0', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
