8f2c407eeec52e5398b1c6ba8d6e32ecbb51fb57
Mathieu7483 <11436@holbertonstudents.com>
2025-09-29 11:46:01 +0200

    Refactor file existence check to use pathlib for improved readability

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Script that add all the arguments in command line"""
 import sys
-import os
+from pathlib import Path
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -9,15 +9,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 args = sys.argv
-if os.path.exists(filename):
-    try:
-        my_list = load_from_json_file(filename)
-        if not isinstance(my_list, list):
-            my_list = []
-    except (json.JSONDecodeError, FileNotFoundError):
-        my_list = []
+if Path(filename).exists():
+    my_list = load_from_json_file(filename)
 else:
     my_list = []
-    
+
 my_list.extend(args[1:])
 save_to_json_file(my_list, filename)
