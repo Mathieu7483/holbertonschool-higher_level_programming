52dfbe78d7d89f92ad754e9f4fae82b599f34882
Mathieu7483 <11436@holbertonstudents.com>
2025-09-29 13:13:29 +0200

    Add Student class with JSON serialization capabilities

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that define a class student """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """function that returns the dictionary description
+        with simple data structure (list, dictionary, string,
+         integer and boolean) for JSON serialization of an object:"""
+        if isinstance(attrs, list):
+            new_dict = {}
+            for name in attrs:
+                if name in self.__dict__:
+                    new_dict[name] = self.__dict__[name]
+            return new_dict
+        else:
+            return self.__dict__
