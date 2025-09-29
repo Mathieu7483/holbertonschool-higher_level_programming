090a33ddbeb075ba0be8ce42360a871ad0f0a4ec
Mathieu7483 <11436@holbertonstudents.com>
2025-09-29 14:42:34 +0200

    Refactor append_after function to remove error handling and simplify file operations

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -6,17 +6,13 @@ def append_after(filename="", search_string="", new_string=""):
     """function that inserts a line of text to a file"""
     new_content = ""
 
-    try:
-        with open(filename, encoding="utf-8") as f:
-            for line in f:
-                new_content += line
-                if search_string in line:
-                    new_content += new_string
-    except FileNotFoundError:
-        return
+    with open(filename, encoding="utf-8") as f:
+         for line in f:
+            new_content += line
+            if search_string in line:
+                new_content += new_string
 
-    try:
-        with open(filename, mode='w', encoding="utf-8") as f:
-            f.write(new_content)
-    except Exception as e:
-        print(f"error not writing in file : {e}")
+
+    with open(filename, mode='w', encoding="utf-8") as f:
+        f.write(new_content)
+
