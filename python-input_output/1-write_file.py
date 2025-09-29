c27ae8a395b9539533c52b0a1da2133907e1db24
Mathieu7483 <11436@holbertonstudents.com>
2025-09-29 09:18:54 +0200

    Fix write_file function to correctly open the file and write the provided text

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,6 @@
 def write_file(filename="", text=""):
     """function that write a file"""
 
-    with open(filename="", text="") as f:
-        f.write(filename)
+    with open(filename, mode='w', encoding="utf-8") as f:
+        num_written = f.write(text)
+        return num_written
