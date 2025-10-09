3a71191801f26331d8d71b4ff3e60854c864330b
Mathieu7483 <11436@holbertonstudents.com>
2025-10-09 11:26:38 +0200

    Remove unused import of get_jwt from JWTManager in task_05_basic_security.py

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -7,7 +7,7 @@ Implements role-based access control
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import (
-    JWTManager, create_access_token, jwt_required, get_jwt_identity, get_jwt
+    JWTManager, create_access_token, jwt_required, get_jwt_identity,
 )
 from werkzeug.security import generate_password_hash, check_password_hash
 
