34163a43161d7d05641326183254c4c0bbff4398
Mathieu7483 <11436@holbertonstudents.com>
2025-10-20 10:51:54 +0200

    Refactor SQL script to retrieve column information for first_table from INFORMATION_SCHEMA

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,8 @@
 -- script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW first_table;
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    -- Filtrer sur la base de données spécifiée
+    TABLE_SCHEMA = 'hbtn_0c_0'
+    -- Filtrer sur la table 'first_table'
+    AND TABLE_NAME = 'first_table'
