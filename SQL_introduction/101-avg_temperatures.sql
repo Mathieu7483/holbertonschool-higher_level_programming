9c076b1fed0b5f0d11874b79300597bbce6e0cbc
Mathieu7483 <11436@holbertonstudents.com>
2025-10-20 13:19:37 +0200

    Implement code changes to enhance functionality and improve performance

---
+++ SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,11 @@
+-- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending
+
+SELECT 
+    city, 
+    AVG(temperature) AS average_temperature
+FROM 
+    weather
+GROUP BY
+    city
+ORDER BY
+    average_temperature DESC;
