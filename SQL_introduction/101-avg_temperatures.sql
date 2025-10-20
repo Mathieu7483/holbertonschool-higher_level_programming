1180853091eddd175553c268a1e0e58749a42566
Mathieu7483 <11436@holbertonstudents.com>
2025-10-20 13:47:40 +0200

    Fix SQL script to correctly order average temperature results by descending order

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,13 +1,10 @@
 -- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending
-USE hbtn_0c_0;
-SOURCE temperatures.sql;
-
-SELECT 
+SELECT
     city,
     ROUND(AVG(value), 4) AS avg_temp
-FROM 
+FROM
     temperatures
 GROUP BY
     city
 ORDER BY
-    avg_temp ASC;
+    avg_temp DESC;
