0d5deeca95ead8185b75a501ed691cd02cdcffbc
Mathieu7483 <11436@holbertonstudents.com>
2025-10-20 12:58:43 +0200

    Add script to change database and table character set to utf8mb4

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,22 @@
+-- Script that changes the character set of the database hbtn_0c_0 to utf8mb4
+-- and changes the character set of the table first_table and its name column to utf8mb4
+-- and utf8mb4_unicode_ci
+ALTER DATABASE 
+    hbtn_0c_0 
+    CHARACTER SET = utf8mb4 
+    COLLATE = utf8mb4_unicode_ci;
+
+
+ALTER TABLE 
+    first_table 
+    CONVERT TO CHARACTER SET utf8mb4 
+    COLLATE utf8mb4_unicode_ci;
+
+
+ALTER TABLE 
+    first_table 
+    MODIFY name 
+    VARCHAR(256) 
+    CHARACTER SET utf8mb4 
+    COLLATE utf8mb4_unicode_ci 
+    NOT NULL;
