11f45fc61b821679dbd0805380c5ffcd596b3ce2
Mathieu7483 <11436@holbertonstudents.com>
2025-10-20 10:42:48 +0200

    Fix formatting in 4-first_table.sql by capitalizing the comment

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
---script that creates a table called first_table in the current database in your MySQL server
+-- Script that creates a table called first_table in the current database in your MySQL server
 CREATE TABLE IF NOT EXISTS first_table (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
