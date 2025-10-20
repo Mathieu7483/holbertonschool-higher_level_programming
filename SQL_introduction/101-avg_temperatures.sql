afca6454634214e01623ac02bffcc5af79e187cc
Mathieu7483 <11436@holbertonstudents.com>
2025-10-20 13:28:55 +0200

    Fix column name in average temperature calculation from 'temperature' to 'value'

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -3,8 +3,8 @@ USE hbtn_0c_0;
 SOURCE temperatures.sql;
 
 SELECT 
-        city,
-    AVG(temperature) * (9/5) + 32 AS avg_temp_f
+    city,
+    AVG(value) * (9/5) + 32 AS avg_temp_f
 FROM 
     temperatures
 GROUP BY
