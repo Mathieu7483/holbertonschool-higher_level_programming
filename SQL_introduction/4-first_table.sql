caca180ef4ebe6c2418f13cd863c288e59ef3aa3
Mathieu7483 <11436@holbertonstudents.com>
2025-10-20 10:37:35 +0200

    Add script to create first_table in the current database

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+--script that creates a table called first_table in the current database in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+); 
