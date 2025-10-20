d825c0f38db43714f47412bbab083b7a7124805c
Mathieu7483 <11436@holbertonstudents.com>
2025-10-20 12:40:22 +0200

    Fix alias name in score counting query in second_table

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 -- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
 SELECT 
     score, 
-    COUNT(*) AS number_of_records
+    COUNT(*) AS number
 FROM 
     second_table
 GROUP BY 
