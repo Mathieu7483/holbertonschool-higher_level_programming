d905d7dff14e0ae4381a488eb5412a979ea6ad48
Mathieu7483 <11436@holbertonstudents.com>
2025-10-20 13:23:35 +0200

    Update average temperature query to calculate Fahrenheit and rename column

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,11 +1,11 @@
 -- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending
 
 SELECT 
-    city, 
-    AVG(temperature) AS average_temperature
+        city,
+    AVG(temperature) * (9/5) + 32 AS avg_temp_f
 FROM 
     temperatures
 GROUP BY
     city
 ORDER BY
-    average_temperature DESC;
+    avg_temp_f DESC;
