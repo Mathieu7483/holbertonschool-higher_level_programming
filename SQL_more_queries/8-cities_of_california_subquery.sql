44a24d05afdc66707e2d734aa590e3a2dcee45b3
Mathieu7483 <11436@holbertonstudents.com>
2025-10-21 10:02:12 +0200

    Refactor SQL query to simplify city and state ID references

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
 -- Script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.id, name
+SELECT id, name
 FROM cities
-WHERE cities.state_id = (
-    SELECT states.id
+WHERE state_id = (
+    SELECT id
     FROM states
-    WHERE states.name = 'California'
-);
-ORDER BY cities.id ASC;
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
