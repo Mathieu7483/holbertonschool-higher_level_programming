e47eb47d63b11e05a8744cb2624692af753e5dd2
Mathieu7483 <11436@holbertonstudents.com>
2025-10-21 08:59:57 +0200

    Add IF NOT EXISTS clause to force_name table creation script

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- script that creates the table force_name on your MySQL server.
 
-CREATE TABLE force_name (
+CREATE TABLE IF NOT EXISTS force_name (
     id INT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
