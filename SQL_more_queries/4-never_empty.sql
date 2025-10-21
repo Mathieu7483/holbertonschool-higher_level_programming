94b9e0e24c4f9efe38fb7d9f43778a0c4b7527a6
Mathieu7483 <11436@holbertonstudents.com>
2025-10-21 09:06:51 +0200

    Add AUTO_INCREMENT and PRIMARY KEY to id column in id_not_null table creation script

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 --  Script that creates the table id_not_null on your MySQL server
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256)
 );
