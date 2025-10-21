19bd0390065763c1c6965625b46bc126ced2e54e
Mathieu7483 <11436@holbertonstudents.com>
2025-10-21 13:37:55 +0200

    Fix SQL script to correctly select show titles instead of genre titles for non-Comedy shows

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,10 @@
 -- Script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
-SELECT tv_genres.title
+SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name != 'Comedy'
+WHERE tv_shows.id NOT IN (
+    SELECT tv_show_genres.show_id
+    FROM tv_show_genres
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
 ORDER BY tv_shows.title ASC;
