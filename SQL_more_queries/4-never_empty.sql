afcb49e166ec2d0755607cd1540b6d54cb7bd4db
Mathieu7483 <11436@holbertonstudents.com>
2025-10-21 09:10:28 +0200

    Fix id column definition in id_not_null table to use NOT NULL constraint and remove AUTO_INCREMENT and PRIMARY KEY

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 --  Script that creates the table id_not_null on your MySQL server
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT with the default value 1 AUTO_INCREMENT PRIMARY KEY,
+    id INT DEFAULT 1 NOT NULL,
     name VARCHAR(256)
 );
