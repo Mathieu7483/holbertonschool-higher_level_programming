ad2a45ad7aada8bfdff9995fc7a24d4cc46c4001
Mathieu7483 <11436@holbertonstudents.com>
2025-10-21 09:55:31 +0200

    Fix SQL query to include city ID in the selection of California cities

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,9 @@
 -- Script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.name
+SELECT id, name
 FROM cities
-WHERE cities.state_id = (
-    SELECT states.id
+WHERE state_id = (
+    SELECT id
     FROM states
-    WHERE states.name = 'California'
+    WHERE name = 'California'
 );
+ORDER BY id ASC;
