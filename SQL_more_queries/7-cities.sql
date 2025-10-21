4dc1b14595c8028dc62e4d7f4e37d4ee57922e89
Mathieu7483 <11436@holbertonstudents.com>
2025-10-21 09:45:11 +0200

    Fix column name for foreign key reference in cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,7 +3,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
-    state_id INT NOT NULL,
-    CONSTRAINT fk_state
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    states_id INT NOT NULL,
+    FOREIGN KEY (states_id) REFERENCES states(id)
 );
