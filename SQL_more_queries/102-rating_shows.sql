1f1417bc5c1a0587bf780a38baf8ef2eae774129
Mathieu7483 <11436@holbertonstudents.com>
2025-10-21 13:53:05 +0200

    Fix SQL script to correctly sum ratings and reference the appropriate table for show ratings

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, SUM (tv_show_rate)
+SELECT tv_shows.title, rating sum
 FROM tv_shows
-JOIN tv_show_rate ON tv_shows.id = tv_show_rate.show_id
+JOIN tv_show_rating ON tv_shows.id = tv_show_rating.show_id
 GROUP BY tv_shows.title
-ORDER BY rate DESC;
+ORDER BY rating DESC;
