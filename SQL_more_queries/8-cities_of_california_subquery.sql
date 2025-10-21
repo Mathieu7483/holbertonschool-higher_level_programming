71f2409b893e7d2d73b07bb7e46c8677d4579bef
Mathieu7483 <11436@holbertonstudents.com>
2025-10-21 10:01:09 +0200

    Fix SQL query to correctly reference cities and states tables

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
 -- Script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT id, name
+SELECT cities.id, name
 FROM cities
-WHERE states_id = (
-    SELECT id
-    FROM state
-    WHERE name = 'California'
+WHERE cities.state_id = (
+    SELECT states.id
+    FROM states
+    WHERE states.name = 'California'
 );
-ORDER BY id ASC;
+ORDER BY cities.id ASC;
