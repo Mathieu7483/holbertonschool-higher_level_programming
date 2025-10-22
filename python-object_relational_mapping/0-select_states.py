0816d8cd71f1a160654c3f67a12c66e801266d77
Mathieu7483 <11436@holbertonstudents.com>
2025-10-22 09:32:22 +0200

    Update script documentation to include usage instructions

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa."""
+"""Script that lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py username password database_name
+"""
 import MySQLdb
 import sys
 
