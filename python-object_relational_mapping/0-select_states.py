f93a7051f57e0371fb75c03beff9a939fda9ecf7
Mathieu7483 <11436@holbertonstudents.com>
2025-10-22 09:20:46 +0200

    Refactor database connection to streamline variable usage

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,13 +6,9 @@ import sys
 
 if __name__ == "__main__":
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
     # Connect to the MySQL database
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     # Create a cursor object
     cursor = db.cursor()
