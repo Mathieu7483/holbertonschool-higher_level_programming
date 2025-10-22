f056e16751771d27bbee150295bd102445d10ce9
Mathieu7483 <11436@holbertonstudents.com>
2025-10-22 09:28:12 +0200

    Refactor database connection to use variables for command line arguments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,14 @@ import sys
 
 if __name__ == "__main__":
 
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
     # Connect to the MySQL database
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=mysql_username, passwd=mysql_password, db=database_name)
 
     # Create a cursor object
     cursor = db.cursor()
