def6ed82ecaac2d4d59a746528cdb5b139e2bbb3
Mathieu7483 <11436@holbertonstudents.com>
2025-11-04 15:26:20 +0100

    Fix missing period in 'Missing size' error message in 8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const size = parseInt(sizeArg);
 const square = 'X';
 
 if (isNaN(size) || size <= 0) {
-  console.log('Missing size.');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
     let line = '';
