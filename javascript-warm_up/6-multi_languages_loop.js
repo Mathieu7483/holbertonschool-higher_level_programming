d37b6527f359236a022af74d61612c4952f9f12c
Mathieu7483 <11436@holbertonstudents.com>
2025-11-04 14:59:08 +0100

    Fix formatting issues in 6-multi_languages_loop.js by adding spaces for consistency

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const stringarr = ['C is fun','Python is cool','JavaScript is amazing'];
-let i=0;
-for (i; i < stringarr.length ; i++){
+const stringarr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+for (i; i < stringarr.length; i++) {
   console.log(stringarr[i]);
 }
