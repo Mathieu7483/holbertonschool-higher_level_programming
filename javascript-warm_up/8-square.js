887306a1a28452afc380e464c362b69dee0502a8
Mathieu7483 <11436@holbertonstudents.com>
2025-11-04 15:23:20 +0100

    Add implementation of square printing based on user-defined size in 8-square.js

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const [, , sizeArg] = process.argv;
+const size = parseInt(sizeArg);
+const square = 'X';
+
+if (isNaN(size) || size <= 0) {
+  console.log('Missing size.');
+} else {
+  for (let i = 0; i < size; i++) {
+    let line = '';
+
+    for (let j = 0; j < size; j++) {
+      line += square;
+    }
+
+    console.log(line);
+  }
+}
