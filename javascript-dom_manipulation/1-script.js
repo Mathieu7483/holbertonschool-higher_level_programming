d1c3b25be3c81d1770865658c49eff033c404874
Mathieu7483 <11436@holbertonstudents.com>
2025-11-10 09:47:55 +0100

    Fix variable name for event listener in 1-script.js

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 document.querySelector('header').style.color = '#FF0000';
-header.addEventListener('click', function() {
+red_header.addEventListener('click', function() {
     this.style.color = '#00FF00';
 });
