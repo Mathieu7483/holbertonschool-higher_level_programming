401dbefeab9b3a331ce7a74164125170b94be155
Mathieu7483 <11436@holbertonstudents.com>
2025-11-10 10:14:53 +0100

    Add initial HTML and JavaScript files for red header click functionality

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const redHeader = document.getElementById('red_header');
+const header = document.querySelector('header');
+
+redHeader.addEventListener('click', function () {
+  header.style.color = '#FF0000';
+});
+header.classList.add('red');
