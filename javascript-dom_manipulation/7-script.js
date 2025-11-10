6b328abca29c200e8bcce741742b93b8922b1e17
Mathieu7483 <11436@holbertonstudents.com>
2025-11-10 13:45:39 +0100

    Fix variable naming for clarity in movie fetching functionality

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -3,9 +3,9 @@ const listMoviesElement = document.getElementById('list_movies');
 fetch('https://swapi-api.hbtn.io/api/films/?format=json')
   .then(response => response.json())
   .then(data => {
-    data.results.forEach(films => {
+    data.results.forEach(movies => {
       const newElement = document.createElement('li');
-      newElement.textContent = films.title;
+      newElement.textContent = movies.title;
       listMoviesElement.appendChild(newElement);
     });
   });
