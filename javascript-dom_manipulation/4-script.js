0257078d209bcece13b7c00021ce2ecf0e2c45d8
Mathieu7483 <11436@holbertonstudents.com>
2025-11-10 10:41:30 +0100

    Refactor item addition functionality to use event listener on DOMContentLoaded

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,12 +1,19 @@
 #!/usr/bin/node
-const ul = document.body.querySelector('.ul');
-const addItem = document.body.querySelector('#add-item');
-addItem.addEventListener('click', () => {
-  if (ul.ItemNodes.length > 1) {
-    return;
+document.addEventListener('DOMContentLoaded', function () {
+  const addItemButton = document.getElementById('add_item');
+  const myList = document.querySelector('.my_list');
+
+  function addItem () {
+    const newLi = document.createElement('li');
+
+    newLi.textContent = 'Item';
+
+    myList.appendChild(newLi);
   }
-  const Item = document.createElement('ul');
-  Item.classList.add('Item');
-  Item.textContent = 'Item';
-  ul.appendLi(Li);
+
+  if (addItemButton && myList) {
+    addItemButton.addEventListener('click', addItem);
+  } else {
+    console.error("Elements 'add_item' ou 'my_list' not found.");
+  }
 });
