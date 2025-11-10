781dc24a68f006b0d468d8e5c695ab5dd57d9f72
Mathieu7483 <11436@holbertonstudents.com>
2025-11-10 13:07:54 +0100

    Add fetch functionality to retrieve and display character name

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const characterElement = document.getElementById('character');
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    characterElement.textContent = data.name;
+  });
