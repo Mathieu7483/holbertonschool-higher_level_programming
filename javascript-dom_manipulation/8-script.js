4c91dfa6c32f4fc7439aa45beb775ecac3165856
Mathieu7483 <11436@holbertonstudents.com>
2025-11-10 13:57:36 +0100

    Simplify fetch response handling by removing unnecessary check for response.ok

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -3,11 +3,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const idElement = document.getElementById('hello');
   fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-    })
+    .then(response => response.json())
     .then(data => {
       idElement.textContent = data.hello;
     });
