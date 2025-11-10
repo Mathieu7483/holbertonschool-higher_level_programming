8cff5b22f4cc4b3489c323b2346e42d0424aaaea
Mathieu7483 <11436@holbertonstudents.com>
2025-11-10 10:46:01 +0100

    Simplify item addition functionality by removing DOMContentLoaded wrapper

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
-document.addEventListener('DOMContentLoaded', function () {
-  const addItemButton = document.getElementById('add_item');
-  const myList = document.querySelector('.my_list');
-
-  function addItem () {
-    const newLi = document.createElement('li');
-
-    newLi.textContent = 'Item';
-
-    myList.appendChild(newLi);
-  }
-
-  if (addItemButton && myList) {
-    addItemButton.addEventListener('click', addItem);
-  }
+document.querySelector('#add_item').addEventListener('click', function () {
+  const newItem = document.createElement('li');
+  newItem.textContent = 'Item';
+  document.querySelector('.my_list').appendChild(newItem);
 });
