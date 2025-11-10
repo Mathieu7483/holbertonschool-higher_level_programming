ce14a568658aef78a58f071163542fc4e2562768
Mathieu7483 <11436@holbertonstudents.com>
2025-11-10 10:36:52 +0100

    Remove unnecessary comments from item addition functionality

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 const ul = document.body.querySelector('.ul');
-
 const addItem = document.body.querySelector('#add-item');
 addItem.addEventListener('click', () => {
-  // Only add a Li if we don't already have one
-  // in addition to the text node "ul"
   if (ul.ItemNodes.length > 1) {
     return;
   }
