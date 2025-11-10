64120529f13fdd43af93a951ecbae5b37bf2aa5f
Mathieu7483 <11436@holbertonstudents.com>
2025-11-10 09:50:41 +0100

    Refactor click event listener to change header color on click

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-document.querySelector('header').style.color = '#FF0000';
-red_header.addEventListener('click', function() {
-    this.style.color = '#00FF00';
+red_header.addEventListener("click", function() {
+    document.querySelector('header').style.color = '#FF0000';
 });
