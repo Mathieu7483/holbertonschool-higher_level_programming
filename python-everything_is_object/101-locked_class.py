fbbfe5b51699cf87cc42704a480134567befde7d
Mathieu7483 <11436@holbertonstudents.com>
2025-11-17 10:27:16 +0100

    Add LockedClass definition with docstring and __slots__ for restricted attributes

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines a locked class with restricted attributes"""
+
+
 class LockedClass:
+    """
+    A class that prevents dynamic creation of instance attributes
+    except for 'first_name'
+    """
     __slots__ = ['first_name']
